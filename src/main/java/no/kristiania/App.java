ffc5ca913c5d4fe5b93730e51dc983b04a477956
Even Gabrielsen <gabrielsen.even@gmail.com>
2019-02-19 19:11:10 +0100

    Update project pom

---
+++ src/main/java/no/kristiania/App.java
@@ -0,0 +1,13 @@
+package no.kristiania;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
