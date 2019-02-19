f6de6103daaac8251ec63d59c42d47f459048b80
Anh Le <anh.q.og.le@gmail.com>
2019-02-19 20:24:07 +0100

    Add pojo objects

---
+++ src/main/java/no/kristiania/db/pojo/Task.java
@@ -0,0 +1,27 @@
+package no.kristiania.db.pojo;
+
+public class Task {
+
+    private String name;
+    private int status;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+
+}
